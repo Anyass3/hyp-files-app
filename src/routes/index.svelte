9c3315d6fa543787148a3ef024602ba8ee9e6ea5
Anyass3 <nyassabu@gmail.com>
2021-10-19 07:17:34 +0000

    rm storageSelect from home

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -12,10 +12,7 @@
 	class="flex h-full flex-col px-2 md:px-8 py-4 gap-8 flex-grow bg-gray-50 border-t border-gray-500 dark:bg-gray-800 rounded-sm dark:text-blue-100 select-none"
 >
 	<h3 class="text-blue-500 dark:text-white text-5xl">Drive Manager</h3>
-	<div class="flex justify-between flex-wrap text-gray-900">
-		<StorageSelect />
-		<!-- <ConnectDrive /> -->
-	</div>
+
 	<div
 		class=" mt-2  p-2 rounded-lg border-gray-500 shadow-sm border-[.3px] text-gray-800 text-xl  dark:text-blue-100"
 	>
