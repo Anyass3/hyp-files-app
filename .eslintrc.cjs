c308be272a33f9e33f556c725741ce41ce43bd96
Abdoulie Nyass <nyassabu@gmail.com>
2023-06-25 17:13:33 +0000

    update configs

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,28 +1,41 @@
 module.exports = {
 	root: true,
+	extends: [
+		'eslint:recommended',
+		'plugin:@typescript-eslint/recommended',
+		'plugin:svelte/recommended',
+		'prettier'
+	],
 	parser: '@typescript-eslint/parser',
-	extends: ['eslint:recommended', 'plugin:@typescript-eslint/recommended', 'prettier'],
-	plugins: ['svelte3', '@typescript-eslint'],
-	ignorePatterns: ['*.cjs'],
-	overrides: [{ files: ['*.svelte'], processor: 'svelte3/svelte3' }],
-	settings: {
-		'svelte3/typescript': () => require('typescript')
-	},
+	plugins: ['@typescript-eslint'],
 	parserOptions: {
 		sourceType: 'module',
-		ecmaVersion: 2020
+		ecmaVersion: 2020,
+		extraFileExtensions: ['.svelte']
 	},
 	env: {
 		browser: true,
 		es2017: true,
 		node: true
 	},
+	overrides: [
+		{
+			files: ['*.svelte'],
+			parser: 'svelte-eslint-parser',
+			parserOptions: {
+				parser: '@typescript-eslint/parser'
+			}
+		}
+	],
 	rules: {
 		'@typescript-eslint/ban-ts-comment': 'warn',
 		'@typescript-eslint/no-empty-function': 'warn',
 		'prefer-const': 'warn',
 		'no-undef': 'warn',
 		'no-empty': 'warn',
-		'@typescript-eslint/no-this-alias': 'warn'
+		'@typescript-eslint/no-this-alias': 'warn',
+		'@typescript-eslint/no-explicit-any': 'warn',
+
+
 	}
 };
